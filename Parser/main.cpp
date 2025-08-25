3d5fd75561378388d3a779df0b52ce4c33139918
Benshan Mei <mbs2021@163.com>
2025-08-26 03:58:26 +0800

    Remove debug output from the main execution flow in the parser. Simplified the success and failure messages for clarity.

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -16,12 +16,8 @@ int main(int argc, char *argv[])
 	string str(argv[1]);
 	Program *program = p.parse(str);
 	if (program != nullptr){
-		printf("解析成功，生成AST\n");
-		printf("%s\n", program->toString().c_str());
-		printf("AST已生成，可以使用解释器执行\n");
 		Interpreter interpreter;
 		interpreter.execute(program);
-		printf("解释器执行完成\n");
 	} else {
 		printf("解析失败\n");
 	}
